052baaa9da27443d2ca2e63933f3c7d88b491209
Cuong Phan <codyreactista@gmail.com>
2023-05-06 13:07:03 +0700

    Clean up code

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "2da25bcc774e4eb9a9e235f2d1305ee7",
+    key: process.env.RAWG_API_KEY,
   },
 });
